5e4e59e8b3ccf198e9038ae1f81a457f65ee92c3
Johannes <jojo.klettern@googlemail.com>
2019-03-17 11:50:54 +0100

    Implementierung Spieler-Klasse und FarbEnum

---
+++ Abalone/src/abalone/FarbEnum.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Gruppe A4
+ * 
+ * Die Enumeration beinhaltet alle Farben,
+ * die ein Spieler annehmen kann.
+ *s
+ */
+package abalone;
+
+public enum FarbEnum {
+	WEISS,
+	SCHWARZ
+}
--- Abalone/src/abalone/Spieler.java
+++ Abalone/src/abalone/Spieler.java
@@ -1,5 +1,86 @@
+/**
+ * @author Gruppe A4
+ * 
+ * Die Klasse Spieler enthält den Plan für die 
+ * Erschaffung eines Spielers.
+ */
 package abalone;
 
 public class Spieler {
 
+	private String name;
+	private FarbEnum farbe;
+	private static FarbEnum farbeZweiterSpieler;
+	/*wird ein Spieler mit einer farbe erzeugt
+	 MUSS der andere Spieler die andere Farbe benutzen
+
+	 */
+
+	/**
+	 * Erschafft ein Spieler Objekt
+	 * 
+	 * @param name Der gewählte Name des Spielers
+	 * @param farbe Die Farbe des Spielers. Beim zweiten Spieler festgelegt
+	 * 
+	 * @return Gibt die erzeugte Spieler-Instanz zurueck
+	 */
+	public Spieler(String name, FarbEnum farbe) {
+		setName(name);
+		setFarbe(farbe);
+	}
+
+	/**
+	 * Setzt das Attribut Name 
+	 * @param name selbstgewaehlter Name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Gibt den gewaehlten Namen zurueck
+	 * 
+	 * @return name selbstgewaehlter Name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * Gibt die Farbe eines Spieler zurueck
+	 * 
+	 * @return farbe 
+	 */
+	public FarbEnum getFarbe() {
+		return this.farbe;
+	}
+
+	/**
+	 * Setzt die Farbe eines Spielers und stellt sicher,
+	 *  dass beide Farben vergeben sind
+	 * 
+	 * @param farbe Aus der Enumeration FarbEnum
+	 * @exception RuntimeException Fehlermeldung bei ungültiger Farbe
+	 */
+	private void setFarbe(FarbEnum farbe) {
+		//Fallunterscheidung: Ausschliessen, dass zwei Spieler dieselbe Farbe haben
+		if (farbeZweiterSpieler == null) {
+			switch (farbe) {
+			case WEISS:
+				this.farbe = farbe;
+				farbeZweiterSpieler = FarbEnum.SCHWARZ;
+				break;
+			case SCHWARZ:
+				this.farbe = farbe;
+				farbeZweiterSpieler = FarbEnum.WEISS;
+				break;
+			default:
+				throw new RuntimeException("Ungültige Farbe");
+			}
+		}else {
+			this.farbe = farbeZweiterSpieler;
+		}
+		
+	}
 }
