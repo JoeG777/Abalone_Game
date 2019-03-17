cb6a28d5e6a0aad882bc930bc57bd4f2603f6717
buni63 <bunyamin.esk@hotmail.com>
2019-03-17 16:22:31 +0100

    Der Klasse Spielzug Konstruktor und getter-Methoden hinzugefügt

--- Abalone/src/abalone/Spielzug.java
+++ Abalone/src/abalone/Spielzug.java
@@ -5,4 +5,22 @@ public class Spielzug {
 	private String von;
 	private String nach;
 	
+	/**
+	 * Konstruktor der Klasse Spielzug
+	 * @param von Die Position der Spieler vor dem Zug
+	 * @param nach Die Position der Spieler nach dem Zug
+	 */
+	public Spielzug(String von, String nach) {
+		this.von = von;
+		this.nach = nach;
+	}
+	
+	public String getSpielzugVon() {
+		return von;
+	}
+	
+	public String getSpielzugNach() {
+		return nach;
+	}
+	
 }
