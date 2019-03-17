1c7680412b8f5bb23f9082c26d3b5cef29c27769
buni63 <bunyamin.esk@hotmail.com>
2019-03-17 16:51:17 +0100

    Historie: Konstruktor und spielzugHinzufuegen-Methode hinzugefügt

--- Abalone/src/abalone/Historie.java
+++ Abalone/src/abalone/Historie.java
@@ -2,4 +2,22 @@ package abalone;
 
 public class Historie {
 
+	private String zuege = "";
+	
+	/**
+	 * Konstruktor der Klasse Historie
+	 * @param zuege String, welcher alle vergangenen Spielzuege enthaelt
+	 */
+	public Historie(String zuege) {
+		this.zuege = zuege;
+	}
+	
+	/**
+	 * Diese Methode dient zum Hinzufuegen eines Spielzugs
+	 * in die Historie nach Abschluss des Spielzuges
+	 */
+	public void spielzugHinzufuegen(Spielzug spielzug) {
+		zuege = zuege + spielzug.getSpielzugVon() + " - " 
+				+ spielzug.getSpielzugNach() + "\n";
+	}
 }
