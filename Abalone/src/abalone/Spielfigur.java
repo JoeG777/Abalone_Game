c0a7ca2bad8ea102c1b4bccfbde73167adcf93d4
jensp <jensp@141.19.146.168>
2019-03-19 16:03:39 +0100

    Namensänderung

--- Abalone/src/abalone/Spielfigur.java
+++ Abalone/src/abalone/Spielfigur.java
@@ -4,8 +4,8 @@ public class Spielfigur {
 
 	private FarbEnum farbe;
 	
-	public Spielfigur(Spielfeld brett, FarbEnum farbe) {
-		if (brett == null) {
+	public Spielfigur(Spielfeld feld, FarbEnum farbe) {
+		if (feld == null) {
 			throw new RuntimeException("Es existiert kein Brett");
 		}
 		setFarbe(farbe);
