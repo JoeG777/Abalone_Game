77be4cd49dcb2de09c4cf94d5dd8bf4f01f15d9f
Julian <juliang98@web.de>
2019-03-23 13:36:01 +0100

    Test fuer Spielbrett hinzugefuegt

--- Abalone/tests/SpielbrettTest.java
+++ Abalone/tests/SpielbrettTest.java
@@ -1,9 +1,13 @@
 //import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 //import org.junit.Test;
+import org.junit.Test;
 
 import abalone.Spielbrett;
+import abalone.Spielfeld;
+import abalone.Spielzug;
 
 public class SpielbrettTest {
 
@@ -12,6 +16,21 @@ public class SpielbrettTest {
 	@BeforeClass
 	public static void setUp() {
 		spielbrett = new Spielbrett();
+	}
+	
+	
+	@Test 
+	public void getAusgangsfelderTest() {
+		Spielzug zug = new Spielzug("C3C4","C4", 0);
+		Spielfeld[] felder = spielbrett.getAusgangsfelder(zug);
+		Spielfeld feld1 = spielbrett.getFeld("C3");
+		Spielfeld feld2 = spielbrett.getFeld("C4");
+		Spielfeld feld3 = spielbrett.getFeld("C5");
+		
+		Spielfeld[] gemeinteFelder= {feld1, feld2};
+		
+		Assert.assertArrayEquals(felder, gemeinteFelder);
+		
 	}
 
 }
