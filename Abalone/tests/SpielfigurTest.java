dbea3c40707ea14096024d25f1b05ee6ba449453
jensp <jensp@141.19.158.79>
2019-03-26 10:22:31 +0100

    fix

---
+++ Abalone/tests/SpielfigurTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import abalone.FarbEnum;
+import abalone.Spielbrett;
+import abalone.Spielfeld;
+import abalone.Spielfigur;
+
+public class SpielfigurTest {
+	
+	Spielfeld feld;
+	Spielfigur figur;
+	Spielbrett spielbrett;
+	private String id = "A1";
+	FarbEnum farbe = FarbEnum.WEISS;
+	RuntimeException e = new RuntimeException("Es existiert kein Brett");
+	
+	@Before
+	public void setSpielfigur() {
+		figur = new Spielfigur(new Spielfeld(new Spielbrett(), id), farbe);
+		
+	}
+	@Test
+	public void testGetfarbe() {
+		assertEquals(FarbEnum.WEISS, figur.getFarbe());
+	}
+	
+	@Test
+	public void testSetFarbe() {
+		figur.setFarbe(FarbEnum.SCHWARZ);
+		assertTrue(FarbEnum.SCHWARZ == figur.getFarbe());
+	}
+
+}
