4d908f0db082337055e5bd5de8c7ec489f67159e
Julian <juliang98@web.de>
2019-03-30 12:46:40 +0100

    Historie ueberarbeitet

--- Abalone/src/abalone/Historie.java
+++ Abalone/src/abalone/Historie.java
@@ -1,14 +1,86 @@
 package abalone;
 
-public class Historie {
+import java.util.ArrayList;
 
-	private String zuege = "";
-	private int zaehler = 1;
+public class Historie {
+	
+	private ArrayList<String> zuege;
+	private int zaehler;
 	
 	/**
 	 * Konstruktor der Klasse Historie
 	 */
 	public Historie() {
+		setZuege(new ArrayList<String>());
+		setZaehler(1);
+	}
+	
+	/**
+	 * Gibt das Zuege-Attribut der Historie zurück.
+	 * @return ArrayList aus Strings, in der die Zuege stehen.
+	 */
+	public ArrayList<String> getZuege() {
+		return zuege;
+	}
+	
+	/**
+	 * Setzt das Zuege Attribut der Historie.
+	 * @param zuege ArrayList aus Strings.
+	 */
+	private void setZuege(ArrayList<String> zuege) {
+		this.zuege = zuege;
+	}
+	
+	/**
+	 * Gib die Nummer des naechsten Zuges zurueck.
+	 * @return aktueller Wert des Zaehler Attributs.
+	 */
+	private int getZaehler() {
+		return this.zaehler;
+	}
+	
+	/**
+	 * Setzt das Zaehler Attribut.
+	 * @param nummer Eine Nummer die groeßer als 0 sein muss.
+	 */
+	private void setZaehler(int nummer) {
+		if(nummer < 1) {
+			throw new RuntimeException("Kann nur von eins zaehlen");
+		}
+		
+		this.zaehler = nummer;
+		
+	}
+	
+	/**
+	 * Erhoeht den Zaehler um eins.
+	 */
+	private void incZaehler() {
+		zaehler++;
+	}
+	
+	/**
+	 * Gibt den Zug an der uebergebenen Stelle aus.
+	 * @param nummer Nummer des Zuges in Historie.
+	 * @return Zug in Form eines Strings.
+	 */
+	public String getZug(int nummer) {
+		if(nummer > zuege.size() || nummer <= 0) {
+			return "Diesen Zug gibt es nicht!";
+		}
+		return this.zuege.get(nummer-1);
+	}
+	
+	@Override
+	public String toString()  {
+		StringBuilder sb = new StringBuilder();
+		
+		for(String zug : zuege) {
+			sb.append(zug);
+			sb.append("\n");
+		}
+		
+		return sb.toString();
 	}
 	
 	/**
@@ -18,15 +90,13 @@ public class Historie {
 	 * @param Spielzug Wird der Historie hinzugefügt
 	 */
 	public void spielzugHinzufuegen(Spielzug spielzug) {
-		zuege = zuege + zaehler + ". " + spielzug.getVon() + " - " 
-				+ spielzug.getNach() + "\n";
-		zaehler++;
+		if(spielzug == null) {
+			throw new RuntimeException("Spielzug muss existieren!");
+		}
+		zuege.add(getZaehler() + ". " + spielzug.getVon() + " - " 
+				+ spielzug.getNach());
+		incZaehler();
 	}
-	/**
-	 * Gibt einen String mit allen vergangenen Spielzuegen zurueck
-	 * @return String mit allen vergangenen Spielzuegen
-	 */
-	public String getZuege() {
-		return zuege;
-	}
+	
+	
 }
