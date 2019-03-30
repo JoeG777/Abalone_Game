19a7da930f2f9730d71c3eeebb59090ae56fcc97
Julian <juliang98@web.de>
2019-03-30 11:29:28 +0100

    6/10 Tests hinzugefuegt

--- Abalone/tests/SpielbrettTest.java
+++ Abalone/tests/SpielbrettTest.java
@@ -1,8 +1,11 @@
 //import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -24,139 +27,99 @@ public class SpielbrettTest {
 	}
 
 
-
-	@Test 
-	public void bekommeRichtungTest() {
-		Spielzug zug = new Spielzug("E2E4", "F4");
-		int richtung = spielbrett.bekommeRichtung(zug);
-		assertEquals(1, richtung);
-	}
-
-	@Test 
-	public void getVorderstenSteinTest() {
-		Spielfeld feld1 = spielbrett.getFeld("F6");
-		Spielfeld feld2 = spielbrett.getFeld("E6");
-		Spielfeld feld3 = spielbrett.getFeld("D6");
-
-		Spielfeld[] felder = {feld1, feld2, feld3};
-		Spielfeld vorderstes = spielbrett.getVorderstenStein(felder, 5);
-
-		assertEquals(feld3, vorderstes);
-	}
-
-	@Test 
-	public void getAusgangsfelderTest() {
-		Spielzug zug = new Spielzug("C3C4","C4", 0, FarbEnum.WEISS);
-		Spielfeld[] felder = spielbrett.getAusgangsfelder(zug);
-		Spielfeld feld1 = spielbrett.getFeld("C3");
-		Spielfeld feld2 = spielbrett.getFeld("C4");
-		Spielfeld feld3 = spielbrett.getFeld("C5");
-
-		Spielfeld[] gemeinteFelder= {feld1, feld2};
-
-		Assert.assertArrayEquals(felder, gemeinteFelder);	
-	}
-
-
-
-
-	@Test 
-	/**
-	 * Testet 3zu1 Sumito.
-	 */
-	public void zieheTest() {
-		spielbrett = new Spielbrett();
-		spielbrett.getFeld("E5").setFigur(spielbrett.getFeld("C3").getFigur());
-		spielbrett.getFeld("C3").setFigur(null);
-		
-		spielbrett.getFeld("E4").setFigur(spielbrett.getFeld("C4").getFigur());
-		spielbrett.getFeld("C4").setFigur(null);
-		
-		spielbrett.getFeld("E3").setFigur(spielbrett.getFeld("C5").getFigur());
-		spielbrett.getFeld("C5").setFigur(null);
-		
-		spielbrett.getFeld("E2").setFigur(spielbrett.getFeld("G5").getFigur());
-		spielbrett.getFeld("G5").setFigur(null);
-		
-		spielbrett.getFeld("E1").setFigur(spielbrett.getFeld("G6").getFigur());
-		spielbrett.getFeld("G6").setFigur(null);
-		
-		Spielzug zug = new Spielzug("E3E5", "E2",0, FarbEnum.SCHWARZ);
-		Spielzug[] zuege = {zug};
-		boolean erfolg = spielbrett.ziehe(zuege);
-		
-		
-		assertTrue(erfolg);
-	}
-	
-	/**
-	 * Testet 2zu1 Sumito.
-	 */
-	
-	@Test
-	public void zieheTest1() {
-		spielbrett = new Spielbrett();
-		spielbrett.getFeld("C2").setFigur(spielbrett.getFeld("C3").getFigur());
-		spielbrett.getFeld("C3").setFigur(null);
-		
-		spielbrett.getFeld("D2").setFigur(spielbrett.getFeld("C4").getFigur());
-		spielbrett.getFeld("C4").setFigur(null);
-		
-		spielbrett.getFeld("E2").setFigur(spielbrett.getFeld("C5").getFigur());
-		spielbrett.getFeld("C5").setFigur(null);
-		
-		spielbrett.getFeld("F2").setFigur(spielbrett.getFeld("G5").getFigur());
-		spielbrett.getFeld("G5").setFigur(null);
-		
-		
-		Spielzug zug = new Spielzug("E3C2", "F2", 1, FarbEnum.SCHWARZ);
-		Spielzug[] zuege = {zug};
-		boolean erfolg = spielbrett.ziehe(zuege);
-		
-		assertTrue(erfolg);
-		
-	}
-	
-	/**
-	 * Testet, ob 3 eigene Kugeln 3 gegnerische 
-	 * Schieben koennen (nicht moeglich).
-	 */
-	@Test
-	public void zieheTest2() {
-		spielbrett = new Spielbrett();
-		spielbrett.getFeld("D3").setFigur(spielbrett.getFeld("C3").getFigur());
-		spielbrett.getFeld("C3").setFigur(null);
-		
-		spielbrett.getFeld("E4").setFigur(spielbrett.getFeld("C4").getFigur());
-		spielbrett.getFeld("C4").setFigur(null);
-		
-		spielbrett.getFeld("F5").setFigur(spielbrett.getFeld("C5").getFigur());
-		spielbrett.getFeld("C5").setFigur(null);
-
-		
-		Spielzug zug = new Spielzug("D3F5", "G6",0, FarbEnum.SCHWARZ);
-		Spielzug[] zuege = {zug};
-		boolean erfolg = spielbrett.ziehe(zuege);
-		assertFalse(erfolg);
-	}
-	
 	@Test
 	public void testGetFelderMitFarbe() {
-		assertEquals();
+		ArrayList<Spielfeld> felderMitFarbe = spielbrett.getFelderMitFarbe(FarbEnum.WEISS);
+
+		ArrayList<Spielfeld> gemeinteFelder = new ArrayList<Spielfeld>();
+		String[] idSpielfelder = {"I5", "I6", "I7", "I8", "I9", "G5", "G6", "G7", 
+				"H4", "H5", "H6", "H7", "H8", "H9", };
+		
+	
+		for(String spielfeld : idSpielfelder) {
+			gemeinteFelder.add(spielbrett.getFeld(spielfeld));
+		}
+		assertEquals(felderMitFarbe, gemeinteFelder);
 	}
 	
 	@Test
 	public void testGetFeld() {
-		assertEquals();
+		Spielfeld i9 = spielbrett.getBrett().get("I9");
+		assertEquals(i9, spielbrett.getFeld("I9"));
 	}
 	
 	@Test
-	public void testGetBrett() {
-		assertEquals();
+	public void testGetAusgangsfelder() {
+		Spielzug zug = new Spielzug ("C3C5", "C4");
+		Spielfeld[] ausgangsfelder = spielbrett.getAusgangsfelder(zug);
+		
+		Spielfeld c3 = spielbrett.getFeld("C3");
+		Spielfeld c4 = spielbrett.getFeld("C4");
+		Spielfeld c5 = spielbrett.getFeld("C5");
+		
+		Spielfeld[] gemeinteFelder = {c3,c4,c5};
+		
+		assertArrayEquals(gemeinteFelder, ausgangsfelder);
 	}
 	
 	@Test
-	public void testWeiseKeyFeldZu() {
-		assertEquals();
+	public void testGetAusgangsfelder1() {
+		Spielzug zug = new Spielzug ("F2E1", "G1");
+		Spielfeld[] ausgangsfelder = spielbrett.getAusgangsfelder(zug);
+		
+		Spielfeld e1 = spielbrett.getFeld("E1");
+		Spielfeld f2 = spielbrett.getFeld("F2");
+		
+		Spielfeld[] gemeinteFelder = {f2,e1};
+		
+		assertArrayEquals(gemeinteFelder, ausgangsfelder);
+	}
+	
+	@Test
+	public void testZiehe() {
+		Spielbrett testBrett = new Spielbrett();
+		Spielzug zug1 = new Spielzug("C3", "D3");
+		Spielzug zug2 = new Spielzug("C4", "D4");
+		Spielzug zug3 = new Spielzug("C5", "D5");
+		Spielzug[] zuege = {zug1, zug2, zug3};
+		
+		Spielbrett echtesBrett = new Spielbrett();
+		echtesBrett.getFeld("D3").setFigur(echtesBrett.getFeld("C3").getFigur());
+		echtesBrett.getFeld("D4").setFigur(echtesBrett.getFeld("C4").getFigur());
+		echtesBrett.getFeld("D5").setFigur(echtesBrett.getFeld("C5").getFigur());
+		echtesBrett.getFeld("C3").setFigur(null);
+		echtesBrett.getFeld("C4").setFigur(null);
+		echtesBrett.getFeld("C5").setFigur(null);
+
+		testBrett.ziehe(zuege);
+		
+		String testBrettString = testBrett.toString();
+		String echtesBrettString = echtesBrett.toString();
+		
+		assertEquals(echtesBrettString, testBrettString);
+		
+		
+		
+	}
+	
+	@Test
+	public void testZiehe1() {
+		Spielbrett testBrett = new Spielbrett();
+		Spielzug zug1 = new Spielzug("F2", null);
+		Spielzug[] zuege = {zug1};
+		
+		Spielbrett echtesBrett = new Spielbrett();
+		echtesBrett.getFeld("F2").setFigur(null);
+
+		testBrett.ziehe(zuege);
+		
+		String testBrettString = testBrett.toString();
+		String echtesBrettString = echtesBrett.toString();
+		
+		assertEquals(echtesBrettString, testBrettString);
+	}
+	
+	public void testZiehe2() {
+		
 	}
 }
