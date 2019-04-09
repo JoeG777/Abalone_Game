af0a3eb0ab4f4fd33869af81557190984616c504
buni63 <bunyamin.esk@hotmail.com>
2019-04-09 18:46:55 +0200

    persistenzInterface, persistenzImplSerialisiert und persistenzImplCSV erstellt

---
+++ Abalone/src/abalone/PersistenzImplSerialisiert.java
@@ -0,0 +1,29 @@
+package abalone;
+
+public class PersistenzImplSerialisiert implements PersistenzInterface{
+
+	@Override
+	public void oeffnen() {
+		
+		
+	}
+
+	@Override
+	public void schliessen() {
+		
+		
+	}
+
+	@Override
+	public void lesen() {
+		
+		
+	}
+
+	@Override
+	public void schreiben() {
+		
+		
+	}
+
+}
---
+++ Abalone/src/abalone/PersistenzInterface.java
@@ -0,0 +1,10 @@
+package abalone;
+
+public interface PersistenzInterface {
+	
+	public void oeffnen();
+	public void schliessen();
+	public void lesen();
+	public void schreiben();
+	
+}
