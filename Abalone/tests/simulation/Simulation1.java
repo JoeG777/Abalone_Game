4dd5e9452db9f2a54bf62964a1b80f2235c85e88
Marius1822640 <1822640@users.noreply.github.com>
2019-04-18 11:25:35 +0200

    Kleine Aenderung fuer neue Notation

--- Abalone/tests/simulation/Simulation1.java
+++ Abalone/tests/simulation/Simulation1.java
@@ -2,15 +2,23 @@ package simulation;
 
 import java.util.Scanner;
 
+import abalone.AbaloneException;
 import abalone.Spiel;
+import abalone.spielbrett.SpielfeldException;
 
 public class Simulation1 {
 
 	public static void main(String[] args) {
-		Spiel s = new Spiel();
+		Spiel s = null;
+		try {
+			s = new Spiel();
+		} catch (SpielfeldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Scanner sc = new Scanner(System.in);
 		String[][] zugSimulation = {
-			{"I9G7", "F6"},{"C3C5", "D3"},
+			{"I9G7", "H8"},{"C3C5", "D3"},
 			{"I7G7", "F7"}, {"A1B2", "C3"},
 			{"I8G6", "F5"}, {"A4B4", "C4"},
 			{"H6F6", "E6"}, {"B3D5", "E6"},
@@ -20,12 +28,27 @@ public class Simulation1 {
 			{"G6E6", "D6"}, {"A2", "A3"},//{"B1B2", "C1"},
 			{"F6D6", "C6"}};
 				
-		s.addSpieler("spieler1", "weiss",2);
-		s.addSpieler("spieler2", "schwarz",2);
+		try {
+			s.addSpieler("spieler1", "weiss",2);
+		} catch (AbaloneException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			s.addSpieler("spieler2", "schwarz",2);
+		} catch (AbaloneException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(s.getStatus());
 		
 		for (String zug[] : zugSimulation) {
-			s.ziehe(zug);
+			try {
+				s.ziehe(zug);
+			} catch (AbaloneException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println(s.getStatus());
 			
 			System.out.println("Drücke ENTER zum fortfahren...");
