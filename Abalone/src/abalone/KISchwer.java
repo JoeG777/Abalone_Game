da3d22101c67ac8e240c7139a997dbaa384dc0d4
Julian <juliang98@web.de>
2019-04-18 20:17:44 +0200

    Die schwere KI speichert nun die minmalen Distanzen zur Mitte in einer Hash-Map

--- Abalone/src/abalone/KISchwer.java
+++ Abalone/src/abalone/KISchwer.java
@@ -9,17 +9,49 @@ import abalone.spielbrett.SpielfeldException;
 
 public class KISchwer extends KI {
 	private Spielbrett simulationsbrett;
-	private HashMap<String, Integer> felderStaerke;
+	public HashMap<String, Integer> felderStaerke;
 	private static final long serialVersionUID = 111L;
 
 	public KISchwer(Spiel spiel, FarbEnum farbe) throws SpielfeldException {
 		super(farbe);
 		simulationsbrett = spiel.getSpielbrett();
 		felderStaerke = new HashMap<String, Integer>();
+		initFelderStaerke();
 
 	}
 
 
+	private void initFelderStaerke() {
+		boolean done = false;
+		int schritte = 1;
+		String aktuellesFeld = "E5";
+
+		while(!done)  {
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 4, 1, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 3, schritte-1, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 5, schritte, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 2, schritte, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 0, schritte, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 1, schritte, schritte);
+			aktuellesFeld = laufeUndBewerte(aktuellesFeld, 4, schritte, schritte);
+
+			schritte++;
+
+			if(schritte == 5) {
+				break;
+			}
+
+		}
+	}
+
+	private String laufeUndBewerte(String id, int richtung, int schritte, int wert) {
+		String newID = id;
+		for(int i = 0; i < schritte; i++) {
+			newID = simulationsbrett.getNachbarByIdInRichtung(newID, richtung);
+			felderStaerke.put(newID, wert);
+		}
+		return newID;
+	}
 	private int calcFeldstaerke(String id) {
 		int feldstaerke = 0;
 		return 0;
@@ -29,42 +61,32 @@ public class KISchwer extends KI {
 		return 0;
 	}
 
-	private boolean aufDiagonale(String id) {
-		char buchstabe = id.charAt(0);
-		int zahl = Character.getNumericValue(id.charAt(1));
 
-		if(buchstabe == 'E' || zahl == '5' || buchstabe - 64 == zahl) {
-			return true;
+
+	private class Node{
+		boolean isRoot;
+		Node[] subNodes;
+		int value;
+		Spielbrett brett;
+		Spielzug zug;
+
+		public Node(boolean isRoot, Spielbrett brett, Spielzug zug) {
+			this.isRoot = isRoot;
+			this.brett = brett;
 		}
 
-		return false;
-	}
-
-
-private class Node{
-	boolean isRoot;
-	Node[] subNodes;
-	int value;
-	Spielbrett brett;
-	Spielzug zug;
-
-	public Node(boolean isRoot, Spielbrett brett, Spielzug zug) {
-		this.isRoot = isRoot;
-		this.brett = brett;
-	}
-
-	public void generateSubNodes(Spielzug[] zuege) {
-		subNodes = new Node[zuege.length];
-		for(int i = 0; i<zuege.length; i++) {
-			subNodes[i] = new Node(false, this.brett, zuege[i]);
+		public void generateSubNodes(Spielzug[] zuege) {
+			subNodes = new Node[zuege.length];
+			for(int i = 0; i<zuege.length; i++) {
+				subNodes[i] = new Node(false, this.brett, zuege[i]);
+			}
 		}
+
+		public void computeValue() {
+
+		}
+
 	}
-
-	public void computeValue() {
-
-	}
-
-}
 
 
 
