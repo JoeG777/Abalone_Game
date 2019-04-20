e5f81b2e22f9f2253a86d0bf4d3633d32ae7218d
buni63 <bunyamin.esk@hotmail.com>
2019-04-20 13:20:57 +0200

    Noch unfertige Implementierung der Methoden von "PersistenzImplCSV"

--- Abalone/src/abalone/PersistenzImplCSV.java
+++ Abalone/src/abalone/PersistenzImplCSV.java
@@ -1,6 +1,11 @@
 package abalone;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * <h1>PersistenzImplCSV</h1>
@@ -12,6 +17,8 @@ import java.io.IOException;
 public class PersistenzImplCSV implements PersistenzInterface, java.io.Serializable {
 
 	private static final long serialVersionUID = 101L;
+	private String dateiName;
+	private BufferedReader br = null;
 
 	/**
 	 * Diese Methode bekommt einen Spielstand in Form einer Datei
@@ -20,38 +27,50 @@ public class PersistenzImplCSV implements PersistenzInterface, java.io.Serializa
 	 */
 	@Override
 	public void oeffnen(String dateiName) {
-		
-		
+		this.dateiName = dateiName;
 	}
 
 	/**
 	 * Diese Methode beendet den Speicher- und Ladevorgang vollstaendig
+	 * @throws IOException
 	 */
 	@Override
-	public void schliessen() {
-		
-		
+	public void schliessen() throws IOException {
+		if (br != null)
+			br.close();
 	}
 
 	/**
 	 * Diese Methode liest die Datei aus und gibt das Resultat als Objekt zurueck
-	 * @return gelesenes Objekt
-	 * @throws IOException
+	 * @return String, der alle Informationen enthaelt
+	 * @throws FileNotFoundException, IOException 
+	 * @throws UnsupportedEncodingException
 	 */
 	@Override
-	public Object lesen() throws IOException {
+	public String lesen() throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		String datei = "";
+		String zeile;
 		
-		return null;
+		br = new BufferedReader(
+			new InputStreamReader(new FileInputStream("sav/" + dateiName + ".csv"), "utf-8"));
+	
+		while ((zeile = br.readLine()) != null) {
+			datei = datei + ";" + zeile;
+		}
+		
+		return datei;
 	}
 
 	/**
 	 * Diese Methode beschreibt eine CSV-Datei
-	 * @param Information, die beschrieben werden soll
+	 * @param zuSchreibenderInhalt Information, die beschrieben werden soll
 	 */
 	@Override
-	public void schreiben(Object zuSchreibendesObjekt) {
+	public void schreiben(Object zuSchreibenderInhalt) {
+		if(!(zuSchreibenderInhalt instanceof String))
+			throw new RuntimeException("Fehlerhafte Informationen!");
 		
-		
+		String s = (String) zuSchreibenderInhalt;
 	}
 
 }
