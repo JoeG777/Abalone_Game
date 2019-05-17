295045cfef845c08d2d2e0655a2a4577ad2937d5
buni63 <bunyamin.esk@hotmail.com>
2019-05-17 22:56:03 +0200

    Log-Fenster nun vollständig implementiert

--- Abalone/src/gui/LogFenster.java
+++ Abalone/src/gui/LogFenster.java
@@ -5,30 +5,61 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+/**
+ * Diese Klasse erschafft ein Log-Fenster.
+ * 
+ * @author Gruppe A4
+ *
+ */
 public class LogFenster {
 	private JFrame frame;
 	private String log;
 	
-	public LogFenster(String log) {
+	/**
+	 * Der Konstruktor der Klasse Log-Fenster.
+	 * 
+	 * @param logObj Log-Objekt
+	 */
+	public LogFenster() {
+		Log logObj = new Log();
+		log = logObj.logLesen();
+		setLog(log);
+		
 		frame = new JFrame();
-		this.setLog(log);
 		frame.setSize(720, 480);
 		frame.setLayout(new GridLayout(25,1));
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.logErstellen(frame, log);
+		logAusgeben(frame, log);
 	}
 	
+	/**
+	 * Gibt die Log-Informationen als String zurueck
+	 * 
+	 * @return String, der die Log-Informationen enthaelt
+	 */
 	public String getLog() {
 		return log;
 	}
 	
+	/**
+	 * Setzt den Log.
+	 * 
+	 * @param log String, der die Log-Informationen enthaelt
+	 */
 	private void setLog(String log) {
 		this.log = log;
 	}
 	
-	private void logErstellen(JFrame frame, String log) {
+	/**
+	 * Befuellt das uebergebene Fenster mit Labels, anhand eines
+	 * uebergebenen Strings.
+	 * 
+	 * @param Log-Fenster als JFrame
+	 * @param log String, der die Log-Informationen enthaelt
+	 */
+	private void logAusgeben(JFrame frame, String log) {
 		String[] zeilen = log.split("\n");
 		
 		JLabel titel = new JLabel("---LOG---");
