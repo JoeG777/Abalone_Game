45263f540b870920154e657de5e70dfea7c54cc6
Julian <juliang98@web.de>
2019-06-01 16:39:04 +0200

    Mehr JavaDoc

--- Abalone/src/gui/FeldButtonListener.java
+++ Abalone/src/gui/FeldButtonListener.java
@@ -4,10 +4,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-
+/**
+ * Verwaltet die Action-Listener von Feld-Buttons.
+ *
+ */
 public class FeldButtonListener implements ActionListener{
 
 	@Override
+	/**
+	 * Verwaltet die ActionEvents für Felder. 
+	 */
 	public void actionPerformed(ActionEvent e) {
 		FeldPanel feld = (FeldPanel)((JButton) e.getSource()).getParent();
 		Spielzug.toggleString(feld.getController().getSpielerAmZugFarbe(), feld.getId());
